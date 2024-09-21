8730e3ebab58be70a027376b858d83f353e2e35a
GGBA23 <gonza.ba.2000@gmail.com>
2024-09-21 15:38:51 -0500

    Carga inicial

---
+++ src/main/java/pe/edu/cibertec/patitas_backend_p/PatitasBackendPApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_backend_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasBackendPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasBackendPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_backend_p/PatitasBackendPApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_backend_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasBackendPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
